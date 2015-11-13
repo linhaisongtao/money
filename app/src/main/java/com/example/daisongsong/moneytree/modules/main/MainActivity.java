84df412a26b82f20aa37964bf5bb8d8b2a14158f
daisongsong <daisongsong@rd.com>
2015-11-13 19:34:08 +0800

    添加呼吸动画

--- app/src/main/java/com/example/daisongsong/moneytree/modules/main/MainActivity.java
+++ app/src/main/java/com/example/daisongsong/moneytree/modules/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.daisongsong.moneytree.modules.main;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,7 +24,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTimeAccountTreeView= (TimeAccountTreeView) findViewById(R.id.mTimeAccountTreeView);
+        mTimeAccountTreeView = (TimeAccountTreeView) findViewById(R.id.mTimeAccountTreeView);
         mTextViewAddNewAccount = (TextView) findViewById(R.id.mTextViewAddNewAccount);
         mTextViewAddNewAccount.setOnClickListener(this);
     }
@@ -32,7 +33,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.mTextViewAddNewAccount: {
-                startActivityForResult(new Intent(this, AddAccountActivity.class), Constant.REQUEST_CODE_ADD_NEW_ACCOUNT);
+                startActivityForResult(new Intent(this, AddAccountActivity.class), Constant
+                        .REQUEST_CODE_ADD_NEW_ACCOUNT);
                 break;
             }
             default:
@@ -41,15 +43,45 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        startAnimator();
+    }
+
+    @Override
+    protected void onPause() {
+        stopAnimator();
+        super.onPause();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case Constant.REQUEST_CODE_ADD_NEW_ACCOUNT:{
-                if(resultCode == RESULT_OK){
+        switch (requestCode) {
+            case Constant.REQUEST_CODE_ADD_NEW_ACCOUNT: {
+                if (resultCode == RESULT_OK) {
                     mTimeAccountTreeView.requestFirstPage();
                 }
                 break;
             }
         }
+    }
+
+    private void startAnimator() {
+        long duration = 2 * 1000;
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(mTextViewAddNewAccount, "scaleX", 0.9f, 1.1f).setDuration
+                (duration);
+        ObjectAnimator animator2 = ObjectAnimator.ofFloat(mTextViewAddNewAccount, "scaleY", 0.9f, 1.1f).setDuration
+                (duration);
+        animator1.setRepeatCount(-1);
+        animator1.setRepeatMode(ObjectAnimator.REVERSE);
+        animator2.setRepeatCount(-1);
+        animator2.setRepeatMode(ObjectAnimator.REVERSE);
+        animator1.start();
+        animator2.start();
+    }
+
+    private void stopAnimator() {
+        mTextViewAddNewAccount.clearAnimation();
     }
 }
